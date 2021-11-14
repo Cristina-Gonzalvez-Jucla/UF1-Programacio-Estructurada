45d171b8a01e98a7e4ab38b38ac310112f845af4
Kristina ✨ <kristina@Kristinas-MacBook-Pro.local>
2021-11-14 12:52:36 +0100

    Actividades de comprension

---
+++ Activitats_Recap/src/task_1.java
@@ -0,0 +1,26 @@
+/*1. Donat el següent codi sobre condicionals, explica què fa:
+        int x = 3;
+        int y = 4;
+        if (x > y) {
+            System.out.println("X és més gran que Y");
+        } else if (x < y) {
+            System.out.println("Y és més gran");
+        }
+
+a) Tenint el compte el valor de les variables x i y, què imprimirà el programa per
+pantalla?
+b) Si el valor de les variables pot canviar, hi ha algun cas que no estigui
+contemplat? Si és així, quin és i com es podria arreglar? (Escriu els canvis)
+
+* */
+public class task_1 {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 4;
+        if (x > y) {
+            System.out.println("X és més gran que Y");
+        } else if (x < y) {
+            System.out.println("Y és més gran");
+        }
+    }
+}
---
+++ Activitats_Recap/src/task_9.java
@@ -0,0 +1,12 @@
+//9. Donat el següent codi sobre loops, contesta:
+// a) Què fa el codi? És correcte? Es pot millorar? Fes-ho.
+public class task_9 {
+    public static void main(String[] args) {
+        // Creacio de la variable array vector amb una llargada de 5
+        int[] vector = new int[5];
+        // Iteracio de la variable i
+        for (int i = 0; i < vector.length; i++) {
+            System.out.print(vector[i]);
+        }
+    }
+}
